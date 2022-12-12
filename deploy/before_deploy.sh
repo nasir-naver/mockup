95a85b33bbccd0bfb69cf5286d166b2fafd3a2c5
nasir-naver <kkk4054@naver.com>
2022-12-12 18:35:57 +0900

    add sed

--- deploy/before_deploy.sh
+++ deploy/before_deploy.sh
@@ -9,6 +9,7 @@ if [ ! -d ../lendit_upload ]; then
   cp -r deploy/script/* ../lendit_upload_tmp
 
   cd ../lendit_upload_tmp
+  sed -i "s/environment/$DEPLOYMENT_GROUP/g" -i ./restart.sh
   zip -r ../lendit_upload/lendit-outsidebank-$DEPLOYMENT_GROUP-${TRAVIS_COMMIT:0:7} *
   cd ../mockup
 fi
